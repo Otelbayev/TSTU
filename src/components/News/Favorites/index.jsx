1b88c529435d92ac795ef7b302a4db8b1373d799
jasurbek <jasurdev1604@gmail.com>
2024-06-15 15:50:55 +0500

    new

--- src/components/News/Favorites/index.jsx
+++ src/components/News/Favorites/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Icon, Wrap } from "./style";
+import { Content } from "./style";
 
 const Favorites = () => {
   const [data, setData] = useState([]);
@@ -13,27 +13,7 @@ const Favorites = () => {
     getData();
   }, []);
 
-  return (
-    <div>
-      {data.map((e) => (
-        <Wrap key={e.id}>
-          <Wrap.Img>
-            <img loading="lazy" src={e?.img} alt="" />
-          </Wrap.Img>
-          <Wrap.Content>
-            <Wrap.Date>
-              <span className="left">{e?.left}</span>
-              <span className="date">{e?.date}</span>
-            </Wrap.Date>
-            <Wrap.Title>
-              {e?.title} <Icon />
-              <div className="bottom"></div>
-            </Wrap.Title>
-          </Wrap.Content>
-        </Wrap>
-      ))}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Favorites;

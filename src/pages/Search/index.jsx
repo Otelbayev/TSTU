ab57d59d8397920add72f5dff2592e4238650dac
jasurbek <jasurdev1604@gmail.com>
2024-09-18 00:34:26 +0500

    new

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useSearchContext } from "./../../context/SearchContext/index";
 import { useTranslation } from "react-i18next";
 import useAxios from "./../../hooks/useAxios";
-import Cookies from "js-cookie";
 import Loading2 from "../../components/Loading2";
 import { Result } from "antd";
 import { NavLink } from "react-router-dom";
 
 const Search = () => {
-  const { value } = useSearchContext();
+  const { value, setIsOpen } = useSearchContext();
   const { i18n, t } = useTranslation();
   const [data, setData] = useState([]);
 
   const { loading, error, sendRequest } = useAxios();
+
   const getData = async (value) => {
     if (!value.trim()) {
       return;
@@ -29,7 +29,7 @@ const Search = () => {
                 i18n.language
               }?search_text=${value}`,
         headers: {
-          Authorization: `Bearer ${Cookies.get("_token")}`,
+          Authorization: `Bearer ${import.meta.env.VITE_CAPCHA_TOKEN}`,
         },
       });
 
@@ -68,28 +68,88 @@ const Search = () => {
     );
   }
 
+  const toNavigate = (
+    type,
+    id,
+    blog_id,
+    departament_id,
+    persons_data_id,
+    page_id
+  ) => {
+    if (type === "Bloglar" || type === "Blogs") {
+      return (
+        <NavLink
+          to={`/${i18n.language}/blog/${i18n.language === "uz" ? id : blog_id}`}
+          onClick={() => setIsOpen(false)}
+        >
+          {t("Bdirections.btn")}
+        </NavLink>
+      );
+    } else if (type === "Departaments" || type === "Bo'limlar") {
+      return (
+        <NavLink
+          to={`/${i18n.language}/department/${
+            i18n.language === "uz" ? id : departament_id
+          }`}
+          onClick={() => setIsOpen(false)}
+        >
+          {t("Bdirections.btn")}
+        </NavLink>
+      );
+    } else if (type === "Employees" || type === "Xodimlar") {
+      return (
+        <NavLink
+          to={`/${i18n.language}/employee/${
+            i18n.language === "uz" ? id : persons_data_id
+          }`}
+          onClick={() => setIsOpen(false)}
+        >
+          {t("Bdirections.btn")}
+        </NavLink>
+      );
+    } else if (type === "Pages" || type === "Pagelar") {
+      return (
+        <NavLink
+          to={`/${i18n.language}/page/${i18n.language === "uz" ? id : page_id}`}
+          onClick={() => setIsOpen(false)}
+        >
+          {t("Bdirections.btn")}
+        </NavLink>
+      );
+    }
+  };
+
   return (
     <div className="root-container">
       <div className="root-wrapper mt-3">
         <div className="mt-5">
-          {data.map((e, index) => (
-            <div key={index}>
-              {e?.length !== 0 ? <div className="title">{e?.type}</div> : ""}
-              {e?.query_list?.map((item) => (
-                <div
-                  key={item?.id}
-                  className="mx-3 d-flex justify-content-between border border-left-0 border-right-0 p-2"
-                >
-                  <div>
-                    {item?.title
-                      ? item?.title
-                      : `${item?.persons_?.fathers_name} ${item?.persons_?.firstName} ${item?.persons_?.lastName}`}
+          {data.map((e, index) => {
+            return (
+              <div key={index}>
+                {e?.length !== 0 ? <div className="title">{e?.type}</div> : ""}
+                {e?.query_list?.map((item) => (
+                  <div
+                    key={item?.id}
+                    className="mx-3 d-flex justify-content-between border border-left-0 border-right-0 p-2"
+                  >
+                    <div>
+                      {item?.title
+                        ? item?.title
+                        : `${item?.persons_?.fathers_name} ${item?.persons_?.firstName} ${item?.persons_?.lastName}`}
+                    </div>
+                    {toNavigate(
+                      e.type,
+                      item?.id,
+                      item?.blog_id,
+                      item?.departament_id,
+                      item?.persons_data_id,
+                      item?.page_id
+                    )}
                   </div>
-                  <NavLink>{t("Bdirections.btn")}</NavLink>
-                </div>
-              ))}
-            </div>
-          ))}
+                ))}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>

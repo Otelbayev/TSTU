7c8af1d3587f73c7d66367b80b759dead090f59e
Jasurbek <jasurdev1604@gmail.com>
2024-10-03 16:09:21 +0500

    new

--- src/Admin/pages/Study/study-dep.jsx
+++ src/Admin/pages/Study/study-dep.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Wrapper from "../../components/Wrapper";
 import useAxios from "../../../hooks/useAxios";
 import Cookies from "js-cookie";
@@ -10,6 +10,10 @@ import "datatables.net-bs5";
 import "datatables.net";
 import $ from "jquery";
 import loadingGif from "../../../assets/icons/loading.gif";
+import { Input, Select } from "../../components/Generics";
+import { studyYears } from "../../utils/mock";
+import { message } from "antd";
+import axios from "axios";
 
 const Div = styled.div`
   td:last-child {
@@ -30,9 +34,17 @@ const StudyDep = () => {
   const location = useLocation();
   const { id } = useParams();
   const { old_year } = useDateContext();
-  const [updateData, setUpdateData] = useState(null);
+
+  const commentRef = useRef();
+  const [oldYear, setOldYear] = useState(2024);
+  const dateRef = useRef();
+  // const authorRef = useRef();
+  const ballRef = useRef();
+  const fileRef = useRef();
 
   const [data, setDate] = useState([]);
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [docId, setDocId] = useState(null);
 
   const getData = async () => {
     const res = await sendRequest({
@@ -55,7 +67,7 @@ const StudyDep = () => {
 
   useEffect(() => {
     getData();
-  }, [updateData]);
+  }, []);
 
   useEffect(() => {
     if (data?.documents_teacher_?.length) {
@@ -107,14 +119,18 @@ const StudyDep = () => {
                   </div> `;
               }
               return `<div style="display: flex; gap:5px"> 
-               <button class="btn btn-outline-primary" data-toggle="modal"
-                            data-target="#exampleModal" data-id="${data?.id}">
-                            <i class="fa fa-edit"></i>
-                    </button>    
+              
               <button class="btn btn-primary show" data-id="${data?.id}">
                             <i class="fa fa-eye"></i>
                     </button>    
-
+ <button class="btn btn-outline-primary update" data-toggle="modal"
+                            data-target="#exampleModal" data-id="${data?.id}">
+                            <i class="fa fa-edit"></i>
+                    </button>    
+                     <button class="btn btn-outline-danger delete" 
+                            data-id="${data?.id}">
+                            <i class="fa fa-trash"></i>
+                    </button>    
                 </div> `;
             },
           },
@@ -134,11 +150,21 @@ const StudyDep = () => {
         const id = $(this).data("id");
         show(id);
       });
+      $("#example tbody").on("click", ".update", function () {
+        const id = $(this).data("id");
+        getById(id);
+        setIsUpdate(true);
+      });
+      $("#example tbody").on("click", ".delete", function () {
+        const id = $(this).data("id");
+        handleDelete(id);
+      });
 
       return () => {
         table.destroy();
-        $("#example tbody").off("click", ".edit-btn");
-        $("#example tbody").off("click", ".delete-btn");
+        $("#example tbody").off("click", ".show");
+        $("#example tbody").off("click", ".update");
+        $("#example tbody").off("click", ".delete");
       };
     }
   }, [data, location.search]);
@@ -151,12 +177,164 @@ const StudyDep = () => {
     );
   };
 
+  const getById = async (id) => {
+    const res = await sendRequest({
+      method: "get",
+      url: `${
+        import.meta.env.VITE_BASE_URL_API
+      }/documentteacher110setcontroller/getbyiddocumentteacher110setstudydep/${id}`,
+      headers: {
+        Authorization: `Bearer ${Cookies.get("_token")}`,
+      },
+    });
+
+    if (res.status === 200) {
+      setDocId(res.data.id);
+      commentRef.current.value = res.data?.comment;
+      dateRef.current.value = res.data?.fixed_date?.split("T")[0];
+      // authorRef.current.value = res.data?.number_authors;
+      ballRef.current.value = res.data?.score;
+      fileRef.current.value = null;
+      setOldYear(res.data?.old_year);
+    }
+  };
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append("person_id", id);
+    formData.append("old_year", Number(oldYear));
+    formData.append("new_year", Number(oldYear) + 1);
+    formData.append("document_id", 89);
+    formData.append("comment", commentRef?.current?.value);
+    formData.append("fixed_date", dateRef?.current?.value);
+    // formData.append("number_authors", Number(authorRef?.current?.value));
+    formData.append("score", Number(ballRef?.current?.value));
+    formData.append("file_up", fileRef?.current?.files[0] || null);
+
+    try {
+      message.loading({ key: "uin", content: "Yuborilmoqda!" });
+      const response = await axios({
+        method: "post",
+        url: `${
+          import.meta.env.VITE_BASE_URL_API
+        }/documentteacher110setcontroller/createdocumentteacher110setstudydep`,
+        data: formData,
+        headers: {
+          Authorization: `Bearer ${Cookies.get("_token")}`,
+        },
+        params: {
+          person_id: id,
+          old_year: oldYear,
+          new_year: oldYear + 1,
+          document_id: 89,
+          comment: commentRef?.current?.value,
+          fixed_date: dateRef?.current?.value,
+          // number_authors: Number(authorRef?.current?.value),
+          score: Number(ballRef?.current?.value),
+        },
+      });
+
+      if (response.status === 200) {
+        message.success({ key: "uin", content: "Yaratildi!" });
+        commentRef.current.value = "";
+        setOldYear(2024);
+        // authorRef.current.value = null;
+        ballRef.current.value = null;
+        fileRef.current.value = "";
+        dateRef.current.value = "";
+        getData();
+        document.getElementById("closeBtn").click();
+      }
+    } catch (err) {
+      message.error({ key: "uin", content: err?.response?.data || "Error" });
+    }
+  };
+
+  const handleUpdate = async () => {
+    const formData = new FormData();
+    formData.append("old_year", Number(oldYear));
+    formData.append("new_year", Number(oldYear) + 1);
+    formData.append("document_id", 89);
+    formData.append("comment", commentRef?.current?.value);
+    formData.append("fixed_date", dateRef?.current?.value);
+    // formData.append("number_authors", Number(authorRef?.current?.value));
+    formData.append("score", Number(ballRef?.current?.value));
+    formData.append("file_up", fileRef?.current?.files[0] || null);
+
+    try {
+      message.loading({ key: "uin", content: "Yuborilmoqda!" });
+      const response = await axios({
+        method: "put",
+        url: `${
+          import.meta.env.VITE_BASE_URL_API
+        }/documentteacher110setcontroller/updatedocumentteacher110setstudydep/${docId}`,
+        data: formData,
+        headers: {
+          Authorization: `Bearer ${Cookies.get("_token")}`,
+        },
+        params: {
+          old_year: oldYear,
+          new_year: oldYear + 1,
+          document_id: 89,
+          comment: commentRef?.current?.value,
+          fixed_date: dateRef?.current?.value,
+          // number_authors: Number(authorRef?.current?.value),
+          score: Number(ballRef?.current?.value),
+        },
+      });
+
+      if (response.status === 200) {
+        message.success({ key: "uin", content: "Yangilandi!" });
+        commentRef.current.value = "";
+        setOldYear(2024);
+        // authorRef.current.value = null;
+        ballRef.current.value = null;
+        fileRef.current.value = "";
+        dateRef.current.value = "";
+        getData();
+        document.getElementById("closeBtn").click();
+      }
+    } catch (err) {
+      message.error({ key: "uin", content: err?.response?.data || "Error" });
+      
+    }
+  };
+
+  const handleDelete = async (id) => {
+    const g = confirm("Are you sure!");
+    if (g) {
+      const response = await axios({
+        method: "delete",
+        url: `${
+          import.meta.env.VITE_BASE_URL_API
+        }/documentteacher110setcontroller/deletedocumentteacher110setstudydep/${id}`,
+        headers: {
+          Authorization: `Bearer ${Cookies.get("_token")}`,
+        },
+      });
+      if (response.status === 200) {
+        getData();
+        document.getElementById("closeBtn").click();
+      }
+    }
+  };
+
+  const onMaqsadClick = () => {
+    setIsUpdate(false);
+    commentRef.current.value = "";
+    setOldYear(2024);
+    // authorRef.current.value = null;
+    ballRef.current.value = null;
+    fileRef.current.value = "";
+    dateRef.current.value = "";
+  };
+
   return (
     <Wrapper
       title={`${data.person_?.lastName} ${data.person_?.firstName} ${data.person_?.fathers_name}`}
       back={true}
       additional={true}
-      setUpdateData={setUpdateData}
+      onMaqsadClick={onMaqsadClick}
     >
       {data.documents_teacher_?.length ? (
         <Div>
@@ -189,6 +367,97 @@ const StudyDep = () => {
           </tbody>
         </table>
       )}
+      <div>
+        <div
+          className="modal fade "
+          id="exampleModal"
+          tabIndex={-1}
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-lg" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Maqsadli parametr
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form className="row" onSubmit={handleSubmit}>
+                  <Input className="col-md-12" ref={commentRef} label="Tarif" />
+                  <Select
+                    className={"col-md-6"}
+                    label={"O'quv yili"}
+                    options={studyYears}
+                    value={oldYear}
+                    onChange={(e) => setOldYear(e)}
+                  />
+                  <Input
+                    className="col-md-6"
+                    type="date"
+                    label="Sana"
+                    ref={dateRef}
+                  />
+                  {/* <Input
+                    className="col-md-6"
+                    type="number"
+                    label="Avtorlar soni"
+                    ref={authorRef}
+                  /> */}
+                  <Input
+                    className="col-md-6"
+                    type="number"
+                    label="Ball"
+                    ref={ballRef}
+                  />
+                  <Input
+                    className="col-md-6"
+                    type="file"
+                    label="Fayl"
+                    ref={fileRef}
+                  />
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  id="closeBtn"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Yopish
+                </button>
+                {isUpdate ? (
+                  <button
+                    type="button"
+                    onClick={handleUpdate}
+                    className="btn btn-primary"
+                  >
+                    Yangilash
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={handleSubmit}
+                    className="btn btn-success"
+                  >
+                    Qo'shish
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
